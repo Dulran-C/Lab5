76764597c4affe97f8e0fbed5fb2bed3e7eb358d
Dulran-C <g00466374@atu.ie>
2026-03-02 16:56:15 +0000

    Update data-service.ts

--- src/app/Services/data-service.ts
+++ src/app/Services/data-service.ts
@@ -8,12 +8,12 @@ import { Observable } from 'rxjs';
 export class DataService {
   constructor(private httpClient:HttpClient){}
 
-  GetStudentData():Observable<any>{
+  GetStudentData():Observable<any>{ <!--method used to fetch data from external api-->
     return this.httpClient.get('https://api.jsonblob.com/019caf2a-b942-7422-8261-bc1893bf772c')
 
   }
 
-  GetWeatherData():Observable<any>{
+  GetWeatherData():Observable<any>{ <!--mwthod used to fetch data from weatherMap , both return Observables-->
     return this.httpClient.get('https://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303')
   }
 
